b04209c7f0ba4d0aad6bb0850547dcff6e179393
Bartosz Lokaj <bartoszlokaj@gmail.com>
2019-06-06 13:32:23 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import img from "./img/sprite.svg";
+import img2 from "./img/work.jpg";
+const scss = require('./sass/main.scss');
+
+console.log('Hello from app.js');
---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "app.bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "./dist",
+              hmr: process.env.NODE_ENV === "development"
+            }
+          },
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "img/",
+              publicPath: "img/"
+            }
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false
+              },
+              pngquant: {
+                quality: "65-90",
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    stats: "errors-only",
+    open: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Front-End Developers",
+      minify: {
+        collapseWhitespace: true
+      },
+      hash: true,
+      template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "app.css"
+    })
+  ]
+};
