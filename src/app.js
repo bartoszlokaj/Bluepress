b85348dbd6a42706b0e958bddd8f0ae067d64fa6
Bartosz Lokaj <bartoszlokaj@gmail.com>
2019-06-07 12:07:50 +0200

    section about work

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import img from "./img/sprite.svg";
 import img2 from "./img/work.jpg";
+import img3 from "./img/cyclone.png";
 const scss = require('./sass/main.scss');
 
 console.log('Hello from app.js');
