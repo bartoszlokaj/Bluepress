6c6d64fc66ba2ad9a0d0ea9334b7d7273ae66919
Bartosz Lokaj <bartoszlokaj@gmail.com>
2019-06-07 13:46:26 +0200

    Tweaking tiles. Secition about

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 import img from "./img/sprite.svg";
 import img2 from "./img/work.jpg";
+import imgWebPack from "./img/webpack.png";
+import imgHtml from "./img/html-icon.png";
+import imgCss from "./img/css-icon.png";
+import imgJs from "./img/js-icon.png";
+import imgPhotoshop from "./img/photoshop-icon.png";
+import imgSass from "./img/sass-icon.png";
 import img3 from "./img/cyclone.png";
 const scss = require('./sass/main.scss');
 
