7b184709b6ae997112eafbc0566335d6d30d58ee
Bartosz Lokaj <bartoszlokaj@gmail.com>
2019-06-12 16:11:39 +0200

    fixed cards positions. Section about

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import img from "./img/sprite.svg";
 import img2 from "./img/work.jpg";
+// import imgChainedPattern from ".img/chained-pattern.png";
 import imgWebPack from "./img/webpack.png";
 import imgHtml from "./img/html-icon.png";
 import imgCss from "./img/css-icon.png";
@@ -7,7 +8,6 @@ import imgJs from "./img/js-icon.png";
 import imgPhotoshop from "./img/photoshop-icon.png";
 import imgSass from "./img/sass-icon.png";
 import img3 from "./img/cyclone.png";
-// import imgTriangles from "./img/triangles.png";
 const scss = require('./sass/main.scss');
 
 console.log('Hello from app.js');
