fb8f9975fb5ffd1239c96c55ead4808cb3eff8c4
Bartosz Lokaj <bartoszlokaj@gmail.com>
2019-06-14 15:33:56 +0200

    setting background image section teh

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
+// IMAGES
 import imgSvg from "./img/sprite.svg";
+//section hero images
 import imgWork from "./img/work.jpg";
+//section about images
 import imgChainedPattern from "./img/chained-pattern.png";
+import imgTriangles from './img/triangles.png';
+//section technology images
+import imgStars from './img/stars.jpg';
 import imgWebPack from "./img/webpack.png";
 import imgHtml from "./img/html-icon.png";
 import imgCss from "./img/css-icon.png";
@@ -8,6 +14,8 @@ import imgJs from "./img/js-icon.png";
 import imgPhotoshop from "./img/photoshop-icon.png";
 import imgSass from "./img/sass-icon.png";
 import imgCyclone from "./img/cyclone.png";
+
+
 const scss = require('./sass/main.scss');
 
 console.log('Hello from app.js');
