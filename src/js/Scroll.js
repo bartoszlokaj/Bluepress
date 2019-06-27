d46312618c8e3701de4ad997e8e45bf7a751777e
Bartosz Lokaj <bartoszlokaj@gmail.com>
2019-06-27 12:02:30 +0200

    small fixes

--- src/js/Scroll.js
+++ src/js/Scroll.js
@@ -1,26 +1,50 @@
-window.onscroll = function() {active()};
+window.onscroll = function() {
+  active();
+};
 
 const backdrop = document.querySelector(".backdrop");
-const sidedrawer = document.querySelector('.sidedrawer');
+const sidedrawer = document.querySelector(".sidedrawer");
 
 let link = document.querySelectorAll(".link");
 
+// ACTIVE LINKS
+// THERE HAS TO BE A WAY TO SHORTEN THIS UP!!
+//////////////////////////////////////////////
 let active = () => {
-  let heroLink = document.querySelectorAll(".link--hero");
+  let homeLink = document.querySelectorAll(".link--home");
   let aboutLink = document.querySelectorAll(".link--about");
   let techLink = document.querySelectorAll(".link--tech");
-  let offerLink = document.querySelectorAll('.link--offer');
+  let offerLink = document.querySelectorAll(".link--offer");
 
   let heroPosition = document.querySelector(".section__hero");
   let aboutPosition = document.querySelector(".section__about");
   let techPosition = document.querySelector(".section__tech");
-  let offerPosition = document.querySelector('.section__offer');
-  
-  let heroHeight = heroPosition.height;
-  console.log(heroHeight);
+  let offerPosition = document.querySelector(".section__offer");
 
+  const activeHome = () => {
     if (
-      document.body.scrollTop > aboutPosition.getBoundingClientRect().top 
+      document.body.scrollTop >
+      heroPosition.getBoundingClientRect().top - 1
+    ) {
+      homeLink.forEach(e => {
+        e.classList.add("active");
+      });
+    }
+    if (
+      document.body.scrollTop >
+      aboutPosition.getBoundingClientRect().top - 1
+    ) {
+      homeLink.forEach(e => {
+        e.classList.remove("active");
+      });
+    }
+  };
+  activeHome();
+
+  const activeAbout = () => {
+    if (
+      document.body.scrollTop >
+      aboutPosition.getBoundingClientRect().top - 1
     ) {
       aboutLink.forEach(e => {
         e.classList.add("active");
@@ -30,18 +54,75 @@ let active = () => {
         e.classList.remove("active");
       });
     }
-  
+    if (
+      document.body.scrollTop >
+      techPosition.getBoundingClientRect().top - 1
+    ) {
+      aboutLink.forEach(e => {
+        e.classList.remove("active");
+      });
+    }
+  };
+  activeAbout();
+
+  const activeTech = () => {
+    if (
+      document.body.scrollTop >
+      techPosition.getBoundingClientRect().top - 1
+    ) {
+      techLink.forEach(e => {
+        e.classList.add("active");
+      });
+    } else {
+      techLink.forEach(e => {
+        e.classList.remove("active");
+      });
+    }
+    if (
+      document.body.scrollTop >
+      offerPosition.getBoundingClientRect().top - 1
+    ) {
+      techLink.forEach(e => {
+        e.classList.remove("active");
+      });
+    }
+  };
+  activeTech();
+
+  const activeOffer = () => {
+    if (
+      document.body.scrollTop >
+      offerPosition.getBoundingClientRect().top - 1
+    ) {
+      offerLink.forEach(e => {
+        e.classList.add("active");
+      });
+    } else {
+      offerLink.forEach(e => {
+        e.classList.remove("active");
+      });
+    }
+    if (
+      document.body.scrollTop >
+      offersPosition.getBoundingClientRect().top - 1 // Needed change after adding more sections!
+    ) {
+      offerLink.forEach(e => {
+        e.classList.remove("active");
+      });
+    }
+  };
+  activeOffer();
 };
 
 link.forEach(e => {
-  e.addEventListener('click', () => {
+  e.addEventListener("click", () => {
     close();
-  })
-})
+  });
+});
 
 const close = () => {
   backdrop.style.animation = "backdrop-out .3s ease-in-out";
   sidedrawer.style.animation = "move-to-left .3s ease-in-out forwards";
-}
+};
 
 export default scroll;
