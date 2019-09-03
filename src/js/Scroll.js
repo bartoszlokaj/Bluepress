5be20a3ffe823ab640a3dbbf935034fbf92e12ff
Bartosz Lokaj <bartoszlokaj@gmail.com>
2019-09-03 12:23:02 +0200

    new build

--- src/js/Scroll.js
+++ src/js/Scroll.js
@@ -2,11 +2,26 @@ window.onscroll = function() {
   active();
 };
 
-const backdrop = document.querySelector(".backdrop");
 const sidedrawer = document.querySelector(".sidedrawer");
 const topperBtn = document.querySelector(".topper");
+const backdrop = document.querySelector(".backdrop");
 
 let link = document.querySelectorAll(".link");
+
+//Close sidenav after click on link
+//////////////
+link.forEach(e => {
+  e.addEventListener("click", () => {
+    close();
+  });
+});
+
+const close = () => {
+  if(screen.width < 900) {
+    backdrop.style.animation = "backdrop-out .3s ease-in-out";
+    sidedrawer.style.animation = "move-to-left .3s ease-in-out forwards";
+  }
+};
 
 // ACTIVE LINKS
 // THERE HAS TO BE A WAY TO SHORTEN THIS UP!!
@@ -51,7 +66,7 @@ let active = () => {
         e.classList.add("active");
       });
       topperBtn.style.animation = "topper-in .4s ease-in-out forwards";
-    }  else {
+    } else {
       aboutLink.forEach(e => {
         e.classList.remove("active");
       });
@@ -120,17 +135,6 @@ let active = () => {
     }
   };
   activeOffer();
-};
-
-link.forEach(e => {
-  e.addEventListener("click", () => {
-    close();
-  });
-});
-
-const close = () => {
-  backdrop.style.animation = "backdrop-out .3s ease-in-out";
-  sidedrawer.style.animation = "move-to-left .3s ease-in-out forwards";
 };
 
 export default scroll;
